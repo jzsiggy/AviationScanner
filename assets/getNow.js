69e199f521d45e3784a3f59a8815acb54d0b1567
Joao Zsigmond <jzsiggy@gmail.com>
2020-03-30 23:54:46 -0300

    Adding get datetime func

---
+++ assets/getNow.js
@@ -0,0 +1,14 @@
+const getNow = () => {
+  const currentdate = new Date(); 
+  const datetime = currentdate.getDate() + "/"
+                  + (currentdate.getMonth()+1)  + "/" 
+                  + currentdate.getFullYear() + " @ "  
+                  + currentdate.getHours() + ":"  
+                  + currentdate.getMinutes() + ":" 
+                  + currentdate.getSeconds();
+  return datetime;
+}
+
+module.exports = {
+  getNow,
+};
